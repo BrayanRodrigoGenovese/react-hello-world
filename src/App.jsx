7583307a2da5f7d08da550a3d3fe55d2ea541806
Brayan <bittolo.8bit@gmail.com>
2026-02-05 00:11:21 +0100

    Fix initialization errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/img/react.svg";
-import "./assets/css/index.css";
-
 export default function App() {
   return (
     <>
